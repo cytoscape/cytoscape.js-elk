28ca2262e84a1da2ce6bd30d00190d5247f71c78
Eduardo <eduardo.942712@gmail.com>
2021-04-06 10:53:09 -0500

    Persisted tooltip until you hover over another node or click outiside of it

--- src/defaults.js
+++ src/defaults.js
@@ -19,38 +19,27 @@ const defaults = {
   }, // A function that applies a transform to the final node position
   ready: ({ cy }) => {
     cy.on('mouseover', 'node', (evt) => {
+        if (tip) {
+          // remove the older tooltip, so we can render the new one.
+          tip.destroy();
+        }
       const node = evt.target;
       const dummyDomEle = document.createElement('div');
       tip = new Tippy(dummyDomEle, { // tippy props:
         // getReferenceClientRect: generateGetBoundingClientRect(evt.renderedPosition.x, evt.renderedPosition.y), // https://atomiks.github.io/tippyjs/v6/all-props/#getreferenceclientrect
         getReferenceClientRect: generateGetBoundingClientRect(evt.originalEvent.x, evt.originalEvent.y), // https://atomiks.github.io/tippyjs/v6/all-props/#getreferenceclientrect
         trigger: 'manual', // mandatory, we cause the tippy to show programmatically.
-        // offset: [50, 50],
         // your own custom props
         // content prop can be used when the target is a single element https://atomiks.github.io/tippyjs/v6/constructor/#prop
         content: () => {
-          const content = document.createElement('div');
+          const content = document.createElement('span');
           content.innerHTML = node._private.data.pattern || node._private.data.id;
-
           return content;
         },
-        // interactive: true,
-        // offset: ({ placement, popper }) => {
-        //
-        //   if (placement === 'bottom') {
-        //   return [ 0, popper.height / 2 ];
-        //   } else {
-        //     return [10, 10];
-        //   }
-        // },
       });
       tip.show();
     });
-    cy.on('mouseout', 'node', () => {
-      if (tip) {
-        tip.destroy();
-      }
-    });
+
     cy.on('tap', 'node', (evt) => {
       const nodeClicked = evt.target;
       console.log('nodeClicked ->', nodeClicked);
