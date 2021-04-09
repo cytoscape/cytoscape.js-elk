67ee4a843826da912723ed8d2f451793a727ba4b
Jean-Yves Rialhon <jean-yves@e8security.com>
2021-04-09 15:56:27 +0200

    adding parsing status and adjusting layout

--- demo/demo-layered.js
+++ demo/demo-layered.js
@@ -2,13 +2,18 @@ cytoscape({
   container: document.getElementById('cy'),
   //elements: fetch('example-graphs/planar-chain.json').then((res) => res.json()),
   elements: fetch('example-graphs/windows_cyto6624.json').then((res) => res.json()),
-  // elements: fetch('example-graphs/windows_cyto50.json').then((res) => res.json()),
+  //elements: fetch('example-graphs/windows_cyto25.json').then((res) => res.json()),
   autoungrabify: true,
   autounselectify: true,
+  zoom: 0.05,
+  minZoom: 0.01,
+  maxZoom: 1e50,
   layout: {
     name: 'elk',
     elk: {
       algorithm: 'layered',
+      direction: 'RIGHT',
+      considerModelOrder: 'NODES_AND_EDGES',
     },
   },
   style: [
@@ -17,9 +22,9 @@ cytoscape({
       style: {
         // label: 'data(id)',
         // 'font-size': '0.5em',
-        'background-color': '#faff79',
-        // 'background-opacity': '0.5',
-        // 'background-blacken': '0.5',
+        //'background-color': '#faff79',
+        'background-opacity': '0.5',
+        'background-blacken': '0.5',
         'border-width': '1',
         // 'border-style': 'solid',
         // 'border-color': 'black',
@@ -29,16 +34,34 @@ cytoscape({
     {
       selector: 'node[isCollapsed != 1]',
       style: {
-        // label: 'data(name)',
+        //label: 'data(name)',
         'font-size': '0.5em',
-        'background-color': '#94aaf3',
-        // 'background-opacity': '0.5',
-        // 'background-blacken': '-0.5',
+        //'background-color': '#94aaf3',
+        'background-opacity': '0.5',
+        'background-blacken': '-0.5',
         'border-width': '1',
         'border-style': 'solid',
         'border-color': 'black'
       },
 
+    },
+    {
+      selector: '.unknown',
+      style: {
+        'background-color': '#94aaf3',
+      },
+    },
+    {
+      selector: '.success',
+      style: {
+        'background-color': '#00ff00',
+      },
+    },
+    {
+      selector: '.failure',
+      style: {
+        'background-color': '#ff0000',
+      },
     },
     {
       selector: 'edge',
